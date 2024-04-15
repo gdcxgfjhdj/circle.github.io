d720e497621dc38212940bc6f4c70d9f737b9914
gdcxgfjhdj <167072459+gdcxgfjhdj@users.noreply.github.com>
2024-04-15 21:02:47 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+//I use chatGPT to assist in creating and building this p5 sketch. I have made improvements and adjustments to the code as necessary 
+
 // Define variables
 let fillColor, x, y, size;
 
